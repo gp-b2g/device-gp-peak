ea53713954ce5fa059468c33f019c6be4073ebd5
Geeksphone <info@geeksphone.com>
2012-10-30 11:15:52 +0100

    First Commit

---
+++ full_twist.mk
@@ -0,0 +1,27 @@
+$(call inherit-product, device/qcom/common/common.mk)
+PRODUCT_COPY_FILES := \
+  device/lge/m4/app_process:system/bin/app_process \
+  device/lge/m4/sensor00fn11.idc:system/usr/idc/sensor00fn11.idc \
+  device/lge/m4/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+  device/lge/m4/vold.fstab:system/etc/vold.fstab \
+
+$(call inherit-product-if-exists, vendor/lge/m4/m4-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/lge/common/vendor-blobs.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  rild.libpath=/system/lib/libril-qc-1.so \
+  rild.libargs=-d/dev/smd0 \
+  ro.use_data_netmgrd=true \
+  ro.moz.ril.simstate_extra_field=true \
+  ro.moz.ril.emergency_by_default=true
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := full_twist
+PRODUCT_DEVICE := twist
+PRODUCT_BRAND := qcom
+PRODUCT_MANUFACTURER := GEEKSPHONE
+PRODUCT_MODEL := GP-TWIST
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
+  persist.usb.serialno=$(PRODUCT_NAME)
