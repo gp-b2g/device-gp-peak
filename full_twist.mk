41145b6198d22504794b51a3db37ec97fa256cd4
Geeksphone <info@geeksphone.com>
2012-12-11 20:09:33 +0100

    Build boot.img from source

--- full_twist.mk
+++ full_twist.mk
@@ -1,11 +1,13 @@
 include device/qcom/msm7627a/msm7627a.mk
 
 PRODUCT_COPY_FILES := \
-  device/geeksphone/twist/app_process:system/bin/app_process \
   device/geeksphone/twist/touch.idc:system/usr/idc/maxtouch-ts154.idc \
   device/geeksphone/twist/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
   device/geeksphone/twist/vold.fstab:system/etc/vold.fstab \
-  device/geeksphone/twist/media_profiles.xml:system/etc/media_profiles.xml
+  device/geeksphone/twist/media_profiles.xml:system/etc/media_profiles.xml \
+  device/geeksphone/twist/init/init.rc:root/init.rc \
+  device/geeksphone/twist/init/init.qcom.usb.rc:root/init.qcom.usb.rc \
+  device/geeksphone/twist/init/ueventd.rc:root/ueventd.rc
 
 $(call inherit-product-if-exists, vendor/geeksphone/twist/vendor-blobs.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
