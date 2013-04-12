6a976d2544ab71e086b87b2281e51a2946db6a8c
Geeksphone <info@geeksphone.com>
2013-04-12 16:31:10 +0200

    Rename to Peak

--- full_peak.mk
+++ full_peak.mk
@@ -1,20 +1,20 @@
 include device/qcom/msm7627a/msm7627a.mk
 
 PRODUCT_COPY_FILES := \
-  device/geeksphone/twist/touch.idc:system/usr/idc/maxtouch-ts154.idc \
-  device/geeksphone/twist/touch.idc:system/usr/idc/himax-touchscreen.idc \
-  device/geeksphone/twist/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-  device/geeksphone/twist/gps.conf:system/etc/gps.conf \
-  device/geeksphone/twist/media_profiles.xml:system/etc/media_profiles.xml \
-  device/geeksphone/twist/init/init.rc:root/init.rc \
-  device/geeksphone/twist/init/init.target.rc:root/init.target.rc \
-  device/geeksphone/twist/init/init.qcom.usb.rc:root/init.qcom.usb.rc \
-  device/geeksphone/twist/init/ueventd.rc:root/ueventd.rc \
-  device/geeksphone/twist/init/charger:root/charger \
-  device/geeksphone/twist/audio.conf:system/etc/bluetooth/audio.conf \
-  device/geeksphone/twist/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
+  device/geeksphone/peak/touch.idc:system/usr/idc/maxtouch-ts154.idc \
+  device/geeksphone/peak/touch.idc:system/usr/idc/himax-touchscreen.idc \
+  device/geeksphone/peak/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+  device/geeksphone/peak/gps.conf:system/etc/gps.conf \
+  device/geeksphone/peak/media_profiles.xml:system/etc/media_profiles.xml \
+  device/geeksphone/peak/init/init.rc:root/init.rc \
+  device/geeksphone/peak/init/init.target.rc:root/init.target.rc \
+  device/geeksphone/peak/init/init.qcom.usb.rc:root/init.qcom.usb.rc \
+  device/geeksphone/peak/init/ueventd.rc:root/ueventd.rc \
+  device/geeksphone/peak/init/charger:root/charger \
+  device/geeksphone/peak/audio.conf:system/etc/bluetooth/audio.conf \
+  device/geeksphone/peak/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
 
-$(call inherit-product-if-exists, vendor/geeksphone/twist/vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/geeksphone/peak/vendor-blobs.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -26,11 +26,11 @@ PRODUCT_PACKAGES += \
   librecovery
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_twist
-PRODUCT_DEVICE := twist
+PRODUCT_NAME := full_peak
+PRODUCT_DEVICE := peak
 PRODUCT_BRAND := qcom
 PRODUCT_MANUFACTURER := GEEKSPHONE
-PRODUCT_MODEL := GP-TWIST
+PRODUCT_MODEL := GP-Peak
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
   persist.usb.serialno=$(PRODUCT_NAME)
