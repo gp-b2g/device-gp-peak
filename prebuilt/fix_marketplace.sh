f96c3ae8ea1e8277bcd9330c1e9652a654fb88c7
Geeksphone <info@geeksphone.com>
2013-08-08 15:18:01 +0200

    Fix browser branding issues on PDI version

--- prebuilt/fix_marketplace.sh
+++ prebuilt/fix_marketplace.sh
@@ -1,4 +1,8 @@
 #!/system/bin/sh
+#For PDI version is needed to delete indexedDB of homescreen to restore the new branding icons
+rm -r /data/local/indexedDB/*homescreen*
+
+#Delete old Marketplace icon
 busybox sed -i '/"marketplace":/,/},/d' /data/local/webapps/webapps.json
 rm -r /data/local/webapps/marketplace
 chmod 666 /system/xbin/fix_marketplace.sh
