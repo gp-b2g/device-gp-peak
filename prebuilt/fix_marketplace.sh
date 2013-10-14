3567126b07669aa8a3b9e4723f2b680e614aebb5
Geeksphone <info@geeksphone.com>
2013-10-14 13:57:00 +0200

    PDI changes now are on PDI branch

--- prebuilt/fix_marketplace.sh
+++ prebuilt/fix_marketplace.sh
@@ -1,7 +1,4 @@
 #!/system/bin/sh
-#For PDI version is needed to delete indexedDB of homescreen to restore the new branding icons
-rm -r /data/local/indexedDB/*homescreen*
-
 #Delete old Marketplace icon
 busybox sed -i '/"marketplace":/,/},/d' /data/local/webapps/webapps.json
 rm -r /data/local/webapps/marketplace
